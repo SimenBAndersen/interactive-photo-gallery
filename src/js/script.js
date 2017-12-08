d4b97fd707245c029db02e16ab50d26aa249aaf8
Simen Bjerkeset Andersen <simen-bandersen@hotmail.no>
2017-12-08 11:17:01 +0100

    search functionality added
    User can now write in the searchfield and the gallery with automatically update with photos that has a caption that matches the searchtext.

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+
+  // All anchor-tags/photos
+  var photos = $('a');
+
+  // Lightbox plugin
   $('.photo-container').magnificPopup({
     delegate: 'a',
     type: 'image',
@@ -7,4 +12,27 @@ $(document).ready(function() {
       titleSrc: 'caption'
     }
   });
+
+  // Search for part of photos' caption
+  // Hide non-matching photos
+  // Show matching photos
+  $('#search').on('input', function(e) {
+
+    // Input value
+    var search = e.target.value.toLowerCase();
+
+    for (var i = 0; i < photos.length; i++) {
+      var caption = photos[i].getAttribute('data-caption');
+
+      // Not a match = hide photo
+      if (!caption.includes(search)) {
+        photos.eq(i).hide();
+      }
+      // Match = show photos
+      if (caption.includes(search)) {
+        photos.eq(i).show();
+      }
+    }
+  });
+
 });
